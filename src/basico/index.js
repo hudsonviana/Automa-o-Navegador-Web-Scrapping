218db5a0de297d30279328ee83e920d598c936d2
hudsonviana <hudson1206@gmail.com>
2022-09-15 16:59:11 -0300

    Tirar o printscreen de uma página web

--- src/basico/index.js
+++ src/basico/index.js
@@ -3,4 +3,14 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    });
+    
+    await page.goto('https://github.com/hudsonviana');
+    await page.screenshot({path:'meugit.png'});
+
+    await browser.close();
 })();
